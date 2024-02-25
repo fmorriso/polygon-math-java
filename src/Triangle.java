d790655250716fd76a082972ffec3b82ab7b2ad9
Frederick Morrison <frederick.morrison@outlook.com>
2024-02-25 14:35:48 -0700

    round permieter and area calculations

--- src/Triangle.java
+++ src/Triangle.java
@@ -9,13 +9,14 @@ public class Triangle extends Polygon{
     //Calculate and return perimeter of triangle
     public double getPerimeter()
     {
-        return getSide1() + getSide2() + getSide3();
+        double perimeter = getSide1() + getSide2() + getSide3();
+        return roundDouble(perimeter, DECIMAL_PLACES_OF_ACCURACY);
     }
  
   //Calculate and return area of triangle using Heron's Formula
     public double getArea() {
         double semiPeri = (getSide1()+getSide2()+getSide3())/2;
-        return  Math.pow(semiPeri * (semiPeri-getSide1()) * (semiPeri-getSide2()) * (semiPeri-getSide3()),0.5);
-      
+        double area =  Math.pow(semiPeri * (semiPeri-getSide1()) * (semiPeri-getSide2()) * (semiPeri-getSide3()),0.5);
+        return roundDouble(area, DECIMAL_PLACES_OF_ACCURACY);
     }
 }      
